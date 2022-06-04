654bd3a5534dad39f2b0918ab1ca150b5ca7f425
Michael-Odhiambo <odhiambomichaelallan@gmail.com>
2022-06-04 11:49:55 +0300

    Latest Changes

---
+++ src/Blackjack/Iteration1/Card.java
@@ -0,0 +1,38 @@
+package Blackjack.Iteration1;
+
+public class Card {
+
+    private Rank rank;
+    private Suit suit;
+    private boolean faceUp;
+
+    // Creates a new card - only use the constants to initialize the card.
+    public Card( Suit suit, Rank rank ) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public Rank getRank() {
+        return this.rank;
+    }
+
+    public void setFaceUp() {
+        this.faceUp = true;
+    }
+
+    public void setFaceDown() {
+        this.faceUp = false;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public String display() {
+        return rank.toString() + suit.toString();
+    }
+}
